825841bdf26e97318ec040d4ee299bec81bee96f
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2024-07-10 14:30:53 +0200

    feat: add security group to the alb and for the ecs cluster

--- security.tf
+++ security.tf
@@ -9,12 +9,35 @@ resource "aws_security_group" "lb_sg" {
         protocol  = "tcp"
         from_port = var.application_port
         to_port   = var.application_port
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        protocol = "-1" # allow all protocols
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Security group for the ECS cluster to control access to the containers from the ALB
+resource "aws_security_group" "ecs_tasks_sg" {
+    name = "ecs-tasks-security-group"
+    vpc_id = aws_vpc.vpc_id
+    description = "security group to control access to and from the ECS tasks"
+
+    ingress {
+        protocol = "tcp"
+        from_port = var.application_port
+        to_port = var.application_port
+        security_groups = [aws_security_group.lb_sg.id]
     }
 
     egress {
         protocol = "-1"
-        
+        from_port = 0 
+        to_port = 0 
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
