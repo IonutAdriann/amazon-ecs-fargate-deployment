99e526981a275388d9ad377d20559d547c10a8e2
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2024-09-16 00:18:41 +0200

    chore: add formatting of the code

--- load_balancer.tf
+++ load_balancer.tf
@@ -4,25 +4,25 @@
 
 
 resource "aws_lb" "ecs_lb" {
-    name = "ecs-load-balancer"
-    subnets = aws_subnet.public.*.id
+    name            = "ecs-load-balancer"
+    subnets         = aws_subnet.public.*.id
     security_groups = [aws_security_group.ecs_sg.id]
 }
 
 resource "aws_lb_target_group" "ecs_target_group_app" {
-    name = "ecs-target-group"
-    port = 80 
-    protocol = "HTTP"
-    vpc_id = aws_vpc.main.id
+    name        = "ecs-target-group"
+    port        = 80 
+    protocol    = "HTTP"
+    vpc_id      = aws_vpc.main.id
     target_type = "ip"
 
     health_check {
-      healthy_threshold = "2" 
-      interval = "30"
-      protocol = "HTTP"
-      matcher = "200"
-      timeout = "3"
-      path = var.check_health_path
+      healthy_threshold   = "2" 
+      interval            = "30"
+      protocol            = "HTTP"
+      matcher             = "200"
+      timeout             = "3"
+      path                = var.check_health_path
       unhealthy_threshold = "2"
     }
 }
@@ -31,11 +31,11 @@ resource "aws_lb_target_group" "ecs_target_group_app" {
 # and forward that from the load balancer to the target group
 resource "aws_lb_listener" "ecs_listener" {
     load_balancer_arn = aws_lb.ecs_lb.arn
-    port = var.container_port
-    protocol = "HTTP"
+    port              = var.container_port
+    protocol          = "HTTP"
 
     default_action {
         target_group_arn = aws_lb_target_group.ecs_target_group_app.arn
-        type = "forward"
+        type             = "forward"
     }
 }
