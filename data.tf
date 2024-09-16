3a707d95e66e7053bc7820d472c1a1d3e1df2d68
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2024-09-16 15:24:03 +0200

    feat: add the vars definted in the container definition policy

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
 data "template_file" "container_definitions" {
     template = file("${path.module}/container_definitions.json")
 
-    variables {
-        ecr_image_url  = var.docker_image
-        fargate_cpu    = var.fargate_container_cpu
-        fargate_memory = var.fargate_container_memory
-        
+    vars = {
+        ecr_image_url    = var.docker_image
+        fargate_cpu      = var.fargate_container_cpu
+        fargate_memory   = var.fargate_container_memory
+        application_port = var.application_port
     }
 }
