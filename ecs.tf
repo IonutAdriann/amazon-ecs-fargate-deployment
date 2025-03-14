99c424b9727d8a6af0edb824a462ab40424ed6f7
Ionut Adrian Rotaru <ionutadriann@Ionuts-MacBook-Pro.local>
2025-03-14 23:36:50 +0200

    feat: add necessary template file for deployment

--- ecs.tf
+++ ecs.tf
@@ -5,3 +5,13 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     name = "application-cluster"
 }
 
+data "template_file" "cb_application" {
+    template = file("./templates/ecs/cb_app.json.tpl")
+
+    vars = {
+        app_image             = var.docker_image
+        app_port              = var.application_port
+        fg_container_cpu      = var.fargate_container_cpu
+        aws_region            = var.aws_region
+    } 
+}
